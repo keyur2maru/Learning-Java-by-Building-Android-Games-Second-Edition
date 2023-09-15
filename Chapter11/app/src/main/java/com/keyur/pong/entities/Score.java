9636739363ed529534067e9b6f3a98a87a65e422
Keyur Maru <kd2maru@gmail.com>
2023-09-15 00:55:13 -0700

    [PONG] Refactoring - Introduce new entity for score keeping logic

--- Chapter11/app/src/main/java/com/keyur/pong/entities/Score.java
+++ Chapter11/app/src/main/java/com/keyur/pong/entities/Score.java
@@ -1,4 +1,30 @@
 package com.keyur.pong.entities;
 
+import com.keyur.pong.utils.GameConstants;
+
 public class Score {
+    // The current score and lives remaining
+    private int mScore = 0;
+    private int mLives = GameConstants.LIVES;
+
+    public void loseLife() {
+        mLives--;
+    }
+
+    public void increaseScore() {
+        mScore++;
+    }
+
+    public int getScore() {
+        return mScore;
+    }
+
+    public int getLives() {
+        return mLives;
+    }
+
+    public void reset() {
+        mScore = 0;
+        mLives = GameConstants.LIVES;
+    }
 }
