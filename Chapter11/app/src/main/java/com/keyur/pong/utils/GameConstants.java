973619395870fd73403e389f22ab8cf31ff40526
Keyur Maru <kd2maru@gmail.com>
2023-09-15 00:54:02 -0700

    [PONG] Refactoring - Introduce GameConstants

--- Chapter11/app/src/main/java/com/keyur/pong/utils/GameConstants.java
+++ Chapter11/app/src/main/java/com/keyur/pong/utils/GameConstants.java
@@ -1,4 +1,13 @@
 package com.keyur.pong.utils;
 
 public class GameConstants {
+    public static final int STOPPED = 0;
+    public static final int LEFT = 1;
+    public static final int RIGHT = 2;
+    public static final int LIVES = 3;
+    public static final int BEEP = 1;
+    public static final int MISS = 2;
+    public static final int BOOP = 3;
+    public static final int BOP = 4;
+
 }
