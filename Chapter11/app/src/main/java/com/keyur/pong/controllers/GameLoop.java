10d3c91dc1ea3ac4b365d2b9a2067cb4076a8e75
Keyur Maru <kd2maru@gmail.com>
2023-09-15 01:22:07 -0700

    [PONG] Add GameLoop to house the game thread

--- Chapter11/app/src/main/java/com/keyur/pong/controllers/GameLoop.java
+++ Chapter11/app/src/main/java/com/keyur/pong/controllers/GameLoop.java
@@ -1,4 +1,59 @@
 package com.keyur.pong.controllers;
 
-public class GameLoop {
+import com.keyur.pong.interfaces.GameEventListener;
+import com.keyur.pong.render.GameRenderer;
+
+public class GameLoop implements Runnable {
+    private volatile boolean running = false;
+    private GameController gameController;
+    private GameRenderer gameRenderer;
+    private GameEventListener gameEventListener;
+
+    private final int MILLIS_IN_SECOND = 1000;
+    // How many frames per second did we get?
+    private long mFPS;
+
+    public GameLoop(GameController gameController, GameRenderer gameRenderer) {
+        this.gameController = gameController;
+        this.gameRenderer = gameRenderer;
+    }
+
+    public void setGameEventListener(GameEventListener listener) {
+        this.gameEventListener = listener;
+    }
+
+    public void start() {
+        running = true;
+        Thread thread = new Thread(this);
+        thread.start();
+
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            // Capture the current time in milliseconds in startFrameTime
+            long frameStartTime = System.currentTimeMillis();
+
+            // Provided the game isn't paused call the update method
+            if (!gameEventListener.isPaused()) {
+                gameController.update(mFPS);
+                gameController.detectCollisions();
+            }
+
+            gameRenderer.draw(true, mFPS);
+
+            // How long did this frame/loop take?
+            // Store the answer in timeThisFrame
+            long timeThisFrame = System.currentTimeMillis() - frameStartTime;
+
+            if (timeThisFrame > 0) {
+                mFPS = MILLIS_IN_SECOND / timeThisFrame;
+            }
+        }
+    }
 }
